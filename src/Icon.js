5ad5d6c1a4a2ba1faa5ed5bd4cd87795be90804a
Nima <nima@movio.co>
2018-10-26 12:54:50 +1300

    export Logo.svg as ReactComponent add main.*.chunk.js of build result, and runtime result

--- src/Icon.js
+++ src/Icon.js
@@ -1,4 +1,6 @@
 // CASE 1
-import {ReactComponent as LogoIcon} from './logo.svg';
-export {LogoIcon};
+// import {ReactComponent as LogoIcon} from './logo.svg';
+// export {LogoIcon};
 
+// CASE 2
+export {ReactComponent as LogoIcon} from './logo.svg';
---
+++ test-results/main.0cd4b7da.chunk-case-2.js
@@ -0,0 +1,37 @@
+(window.webpackJsonp = window.webpackJsonp || []).push([[0], {
+    14: function (e, t, n) {
+    }, 16: function (e, t, n) {
+    }, 18: function (e, t, n) {
+        "use strict";
+        n.r(t);
+        var a = n(0), o = n.n(a), r = n(3), c = n.n(r), l = (n(14), n(4)), i = n(5), s = n(7), p = n(6), d = n(8),
+            m = n(2), u = (n(16), function (e) {
+                function t() {
+                    return Object(l.a)(this, t), Object(s.a)(this, Object(p.a)(t).apply(this, arguments))
+                }
+
+                return Object(d.a)(t, e), Object(i.a)(t, [{
+                    key: "render", value: function () {
+                        return o.a.createElement("div", {className: "App"}, o.a.createElement("header", {className: "App-header"}, o.a.createElement(m.ReactComponent, {
+                            className: "App-logo",
+                            alt: "logo"
+                        }), o.a.createElement("p", null, "Edit ", o.a.createElement("code", null, "src/App.js"), " and save to reload."), o.a.createElement("a", {
+                            className: "App-link",
+                            href: "https://reactjs.org",
+                            target: "_blank",
+                            rel: "noopener noreferrer"
+                        }, "Learn React")))
+                    }
+                }]), t
+            }(a.Component));
+        Boolean("localhost" === window.location.hostname || "[::1]" === window.location.hostname || window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
+        c.a.render(o.a.createElement(u, null), document.getElementById("root")), "serviceWorker" in navigator && navigator.serviceWorker.ready.then(function (e) {
+            e.unregister()
+        })
+    }, 2: function (e, t, n) {
+        e.exports = n.p + "static/media/logo.5d5d9eef.svg"
+    }, 9: function (e, t, n) {
+        e.exports = n(18)
+    }
+}, [[9, 2, 1]]]);
+//# sourceMappingURL=main.0cd4b7da.chunk.js.map
